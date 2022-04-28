b3b8ae2ef9efa844f3c7463a25c5c022202fd6c8
liezsm <liez.dev@gmail.com>
2022-04-28 12:15:33 +0800

    Added a custom hook to fetch movies from api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-// api
-
-import API from "../API";
-
 // config
 
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
@@ -11,38 +7,13 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // components
 
 // hooks
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 // image
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false);
-      setLoading(true);
-
-      const movies = await API.fetchMovies(searchTerm, page);
-
-      setState((prev) => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
-      }));
-    } catch (error) {
-      setError(true);
-    }
-    setLoading(false);
-  };
-  // only run firt component mount, will run once use empty array
-  // exp initial render
-  useEffect(() => {
-    fetchMovies();
-  }, []);
-
+  const { state, loading, error } = useHomeFetch();
   console.log(state);
   return <div>Homepage</div>;
 };
---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+// api
+
+import API from "../API";
+
+const inititalState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0,
+};
+
+export const useHomeFetch = (url) => {
+  // initial
+  const [state, setState] = useState(inititalState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+  // only run firt component mount, will run once use empty array
+  // exp initial render
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return { state, loading, error };
+};

e9269ee4f24cdc21a45876e56d10a4b1f6d66cac
liezsm <liez.dev@gmail.com>
2022-04-29 08:42:37 +0800

    search bar design

--- src/components/SearchBar/SearchBar.styles.js
+++ src/components/SearchBar/SearchBar.styles.js
@@ -1,5 +1,45 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100px;
+  background: var(--darkGrey);
+  padding: 0 20px;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  position: relative;
+  max-width: var(--maxwidth);
+  width: 100%;
+  height: 55px;
+  background: var(--medGrey);
+  margin: 0 auto;
+  border-radius: 40px;
+  color: var(--white);
+
+  img {
+    position: absolute;
+    top: 14px;
+    left: 15px;
+    width: 30px;
+  }
+
+  input[type="text"] {
+    font-size: 20px;
+    position: absolute;
+    left: 0;
+    margin: 8px 0;
+    padding: 0 0 0 60px;
+    border: none;
+
+    width: 95%;
+    background: transparent;
+    height: 40px;
+    color: var(--white);
+
+    :focus {
+      outline: none;
+    }
+  }
+`;
